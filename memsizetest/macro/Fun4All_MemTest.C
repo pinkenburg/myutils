340ecc702932a097dde20cdf85094ad26a1e89a4
Chris Pinkenburg <pinkenburg@bnl.gov>
2019-02-15 11:46:06 -0500

    Add macro to run memory test

---
+++ memsizetest/macro/Fun4All_MemTest.C
@@ -0,0 +1,42 @@
+#if ROOT_VERSION_CODE >= ROOT_VERSION(6,00,0)
+#include <fun4all/SubsysReco.h>
+#include <fun4all/Fun4AllServer.h>
+#include <fun4all/Fun4AllInputManager.h>
+#include <fun4all/Fun4AllDummyInputManager.h>
+#include <fun4all/Fun4AllDstOutputManager.h>
+
+#include <memsizetest/GenerateHits.h>
+#include <memsizetest/GenerateRootHits.h>
+
+#include <phool/recoConsts.h>
+
+R__LOAD_LIBRARY(libfun4all.so)
+R__LOAD_LIBRARY(libmemsizetest.so)
+#endif
+
+void Fun4All_MemTest(const int nevnt = 10)
+{
+  gSystem->Load("libfun4all.so");
+  gSystem->Load("libmemsizetest.so");
+  Fun4AllServer *se = Fun4AllServer::instance();
+  se->Verbosity(1);
+  recoConsts *rc = recoConsts::instance();
+    rc->set_IntFlag("RANDOMSEED", 12345);
+
+    SubsysReco *genhits = new GenerateRootHits();
+    se->registerSubsystem(genhits);
+
+    Fun4AllInputManager *in = new Fun4AllDummyInputManager("JADE");
+    se->registerInputManager(in);
+
+    Fun4AllDstOutputManager *out = new Fun4AllDstOutputManager("DSTOUT", "genhits.root");
+  // if (do_dst_compress) DstCompress(out);
+    se->registerOutputManager(out);
+    se->run(nevnt);
+
+    se->End();
+    delete se;
+    gSystem->Exit(0);
+
+}
+
